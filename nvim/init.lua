70de0e45b2ed33badde599eff66c76b4eb1bd803
Ferdinand Rau <ferdinandlrau@gmail.com>
2023-03-15 20:53:50 +0100

    add basic nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,100 @@
+-------------
+-- Options --
+-------------
+
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.shiftwidth = 0
+opt.expandtab = true
+
+opt.undofile = true
+opt.clipboard = { "unnamedplus" }
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.completeopt = { "menuone", "noinsert" }
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.number = true
+opt.signcolumn = "yes"
+opt.showmode = false
+
+opt.termguicolors = true
+
+opt.scrolloff = 2
+opt.sidescrolloff = 5
+opt.wrap = false
+
+-----------------
+-- Diagnostics --
+-----------------
+
+vim.diagnostic.config({
+    severity_sort = true,
+    float = {
+        style = "minimal",
+        border = "single",
+    },
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
+
+--------------
+-- Mappings --
+--------------
+
+local g = vim.g
+
+g.mapleader = " "
+g.maplocalleader = ","
+
+local function set_normal_keymap(lhs, rhs)
+    vim.keymap.set("n", lhs, rhs, { noremap = true, silent = true })
+end
+
+set_normal_keymap("K", "<C-w><C-k>")
+set_normal_keymap("J", "<C-w><C-j>")
+set_normal_keymap("H", "<C-w><C-h>")
+set_normal_keymap("L", "<C-w><C-l>")
+
+set_normal_keymap("<C-k>", "<Cmd>resize -6<CR>")
+set_normal_keymap("<C-j>", "<Cmd>resize +6<CR>")
+set_normal_keymap("<C-h>", "<Cmd>vertical resize -6<CR>")
+set_normal_keymap("<C-l>", "<Cmd>vertical resize +6<CR>")
+
+set_normal_keymap("<BS>", "<C-o>")
+
+------------------
+-- Autocommands --
+------------------
+
+local group = vim.api.nvim_create_augroup("UserConfig", { clear = true })
+
+local function create_autocmd(events, opts)
+    opts.group = group
+    vim.api.nvim_create_autocmd(events, opts)
+end
+
+create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+create_autocmd("FileType", {
+    pattern = { "gitcommit", "gitrebase" },
+    command = "startinsert | 1",
+})
+
+-------------
+-- Plugins --
+-------------
+
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
